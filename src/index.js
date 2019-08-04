cdbcc7412b89922ad6521508e4d3df6ba0b82d07
ctejedor <tejedorcarlos89@gmail.com>
2019-08-04 21:27:41 +0200

    Adding some scripts to package.json and chaging the structure

--- src/index.js
+++ src/index.js
@@ -1,43 +1,32 @@
 'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-var contador = 0;
+let contador = 0;
 /**
  * Set to lowercase all the values from data.
  * @param  data - The data to lowercase.
  * @param {Function} filter - Filter what values don´t want to lowercase.
  */
-var low = function low(data, filter) {
-  return _handler(data, 'toLowerCase', filter);
-};
+const low = (data, filter) => _handler(data, 'toLowerCase', filter);
 /**
  * Set to uppercase all the values from data.
  * @param  data - The data to uppercase.
  * @param {Function} filter - Filter what values don´t want to uppercase.
  */
-var up = function up(data, filter) {
-  return _handler(data, 'toUpperCase', filter);
-};
+const up = (data, filter) => _handler(data, 'toUpperCase', filter);
 /**
  * Function to handler errors.
  * @param  data - The data to upper/lower-case.
  * @param {*} method method to execute (toUpperCase | toLowerCase)
  * @param {Function} filter - Filter what values don´t want to upper/lower-case.
  */
-var _handler = function _handler(data, method, filter) {
-  var dataUpper = void 0;
+const _handler = (data, method, filter) => {
+  let dataUpper;
   try {
     dataUpper = _lowerUpper(data, method, filter);
   } catch (error) {
     console.error(error);
     dataUpper = data;
   }
-  console.log('Vueltas: ' + contador);
+  console.log(`Vueltas: ${contador}`);
   contador = 0;
   return dataUpper;
 };
@@ -47,11 +36,17 @@ var _handler = function _handler(data, method, filter) {
  * @param {*} method method to execute (toUpperCase | toLowerCase)
  * @param {Function} filter - Filter what values don´t want to upper/lower-case.
  */
-var _lowerUpper = function _lowerUpper(data, method, filter) {
+const _lowerUpper = (data, method, filter) => {
   contador++;
-  var dataUpper = void 0;
-  if (data && (filter && typeof filter === 'function' && !filter(data) || !filter || typeof filter !== 'function')) {
-    if (typeof data === 'string') dataUpper = _lowUp(data, method);else if (Array.isArray(data)) dataUpper = _lowUpArray(data, method);else if ((typeof data === 'undefined' ? 'undefined' : _typeof(data)) === 'object') dataUpper = _lowUpObject(data, method);else dataUpper = data;
+  let dataUpper;
+  if (
+    data &&
+    ((filter && typeof filter === 'function' && !filter(data)) || !filter || typeof filter !== 'function')
+  ) {
+    if (typeof data === 'string') dataUpper = _lowUp(data, method);
+    else if (Array.isArray(data)) dataUpper = _lowUpArray(data, method);
+    else if (typeof data === 'object') dataUpper = _lowUpObject(data, method);
+    else dataUpper = data;
   } else {
     dataUpper = data;
   }
@@ -62,7 +57,7 @@ var _lowerUpper = function _lowerUpper(data, method, filter) {
  * @param  data - The data to upper/lower case.
  * @param {*} method method to execute (toUpperCase | toLowerCase)
  */
-var _lowUp = function _lowUp(value, method) {
+const _lowUp = (value, method) => {
   return value[method]();
 };
 /**
@@ -70,22 +65,19 @@ var _lowUp = function _lowUp(value, method) {
  * @param {*} array
  * @param {*} method method to execute (toUpperCase | toLowerCase)
  */
-var _lowUpArray = function _lowUpArray(array, method) {
-  return array.map(function (el) {
-    return typeof el === 'string' ? _lowUp(el, method) : _lowerUpper(el, method);
-  });
-};
+const _lowUpArray = (array, method) =>
+  array.map(el => (typeof el === 'string' ? _lowUp(el, method) : _lowerUpper(el, method)));
 /**
  * Iterates over the object and upper/lower case the values of all properties
  * @param {*} object
  * @param {*} method method to execute (toUpperCase | toLowerCase)
  */
-var _lowUpObject = function _lowUpObject(object, method) {
-  var objectUpper = {};
-  Object.keys(object).forEach(function (key) {
+const _lowUpObject = (object, method) => {
+  let objectUpper = {};
+  Object.keys(object).forEach(key => {
     objectUpper[key] = _lowerUpper(object[key], method);
   });
   return objectUpper;
 };
-var LowUp = { low: low, up: up };
-exports.default = LowUp;
+const LowUp = { low, up };
+export default LowUp;
--- test/lowup.spec.js
+++ test/lowup.spec.js
@@ -1,4 +1,4 @@
-import LowUp from '../lib/index';
+import LowUp from '../src/index';
 import DataTest from './data';
 let chai = require('chai');
 const { should } = chai;
